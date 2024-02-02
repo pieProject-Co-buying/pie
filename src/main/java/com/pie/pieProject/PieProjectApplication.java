1d37c2efdddd4f1ca7fadd670adfa382f143c6b0
pieProjectMaster <pie030511@gmail.com>
2024-02-02 12:32:46 +0900

    [2024.02.02] 김혜정 - 최초설정 업데이트 - 최초세팅 1. gitIgnore 추가 2. bootstrap 세팅 3. 로고추가 4. index nav 제작 5. static CSS(페이지 공통 스타일 추가)

---
+++ src/main/java/com/pie/pieProject/PieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pie.pieProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pie/pieProject/PieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pie.pieProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
