e5cbc9a7d0351953325bf30a63a75975ef695186
KHJ_laptop <kheong5697@gmail.com>
2024-02-08 03:50:35 +0900

    [2024.02.08] 김혜정 - Thymeleaf 구조 정리 / mybatis+db세팅완료 1. 현재 생성된 front 페이지 index 작업완료 2. mybatis+db 생성 및 로그인 기능으로 테스트 완료

--- src/main/java/com/pie/pieProject/controllers/MemberController.java
+++ src/main/java/com/pie/pieProject/controllers/MemberController.java
@@ -1,5 +1,42 @@
 package com.pie.pieProject.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.pie.pieProject.DAO.IMemberDao;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+
+@Controller
 public class MemberController {
+	@Autowired
+	IMemberDao dao;
+
+	@PostMapping("/loginAction")
+	public String loginProcess(HttpServletRequest request, Model model) {
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		int su = dao.login(id, password);
+		if(su>0) {
+			HttpSession session = request.getSession(true);
+			session.setAttribute("userId", id);
+			return "redirect:/";
+		}else {
+			return "redirect:/login";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String logoutProcess(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		return "redirect:/";
+	}
 
 }
