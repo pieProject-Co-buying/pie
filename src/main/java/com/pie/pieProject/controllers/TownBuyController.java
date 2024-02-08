72063831cad95e3a1271900ca956b9eeb91faa39
GREEN <GREEN@G704-13>
2024-02-08 17:46:27 +0900

    [2024.02.08] 김효정

--- src/main/java/com/pie/pieProject/controllers/TownBuyController.java
+++ src/main/java/com/pie/pieProject/controllers/TownBuyController.java
@@ -1,5 +1,38 @@
 package com.pie.pieProject.controllers;
 
-public class TownBuyController {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pie.pieProject.DAO.ITownBuyBoardDao;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller 
+public class TownBuyController {
+	
+	@Autowired
+	ITownBuyBoardDao dao;
+	
+	
+	
+	@RequestMapping("/townBuySearch")
+	public String toBoardList(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("list", dao.listDao());
+				
+		return "pieContents/townBuySearch";
+		
+		
+	
+	}
+	
+ 
+
+	
+	
 }
+                                                                          
