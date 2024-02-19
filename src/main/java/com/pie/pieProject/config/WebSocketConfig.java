60da02e4ce8135e8822af571d4f9d542686ce046
GREEN <GREEN@G704-13>
2024-02-19 15:48:46 +0900

    [2024.02.19] 김효정
    현재까지 작업분 commit
    
    footer 디자인 수정

--- src/main/java/com/pie/pieProject/config/WebSocketConfig.java
+++ src/main/java/com/pie/pieProject/config/WebSocketConfig.java
@@ -20,6 +20,10 @@ public class WebSocketConfig implements WebSocketConfigurer{
 	
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
 		registry.addHandler(socketHandler, "/chating");
 	}
+	
+	
+	
 }
