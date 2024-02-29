424b77cba2e1b8d3e9fa793b81625b33aa4649d9
GREEN <GREEN@G704-13>
2024-02-29 19:52:46 +0900

    [2024.02.29] 김효정
    기업공구 board crud 진행중

--- src/main/java/com/pie/pieProject/DAO/IBusinessApplyDao.java
+++ src/main/java/com/pie/pieProject/DAO/IBusinessApplyDao.java
@@ -1,11 +1,32 @@
 package com.pie.pieProject.DAO;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Controller;
+
 import com.pie.pieProject.DTO.BusinessApplyDto;
+import com.pie.pieProject.DTO.TownBuyBoardDto;
 
-
+@Mapper
 public interface IBusinessApplyDao {
 	
 	
-	public int bus_apply(BusinessApplyDto dto);
+	//글쓰기
+	public int saveApply(BusinessApplyDto dto);
+	
+	//리스트보기
+	public List<BusinessApplyDto> applyBoard();
+	
+	//상세보기
+	public List<BusinessApplyDto> applyBoardDetail(String sId);
+	
+	
+	//삭제
+	public int deleteDao(String sId);	
+	
+	
+	//수정
+	public int updateDao(BusinessApplyDto dto);
 	
 }
