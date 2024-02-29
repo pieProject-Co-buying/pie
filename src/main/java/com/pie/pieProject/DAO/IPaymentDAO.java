881fcb1091cb9028997604ad2aaefb0e6fc69153
GREEN <GREEN@G704-11>
2024-02-29 20:09:00 +0900

    [2024.02.29] 정지환 - 사용자 결제 내역 페이지 제작(진행 중)

--- src/main/java/com/pie/pieProject/DAO/IPaymentDAO.java
+++ src/main/java/com/pie/pieProject/DAO/IPaymentDAO.java
@@ -15,10 +15,15 @@ public interface IPaymentDAO {
 	public void insertPayment(PaymentDTO dto);
 	// 아이디, 닉네임 기반 검색
 	public ArrayList<PaymentDTO> searchBuyer(String keyword);
+	//
+	public ArrayList<PaymentDTO> buyList(String id);	
 	// 상품 코드와 카테고리로 해당 상품 검색
 	//public PaymentDTO findPay(int num,String category);
 	// 상품 코드와 카테고리로 해당 상품 결제일 검색
 	//public PaymentDTO selectDate(int num,String category);
 	public PaymentDTO payBoard(int num,String category);
 	//public PaymentDTO payCode(int num);
+	
+	public ArrayList<String> buyListpic(String id);
+	public ArrayList<String> buyListpro(String id);
 }
--- src/main/java/com/pie/pieProject/config/WebSecurityConfig.java
+++ src/main/java/com/pie/pieProject/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig {
 						"/proxyBuyApply", "/payCheck", "/townForm", "/proxyApplyForm", "/proxyApplyFormUpload",
 						"/viewProxyApplyBoard", "/proxyApplyupdateForm", "/proxyApplyFormUpdate",
 						"/deleteProxyApplyAction", "/stateUpdateAction", "/townBuyResult", "/shareServiceBoardConsole",
-						"/shareServiceApplyConsole", "/searchBuyerName", "/searchBoardName", "/Follwing","/adminPage")
+						"/shareServiceApplyConsole", "/searchBuyerName", "/searchBoardName", "/Follwing","/adminPage","/shareServicebuyBoard")
 				.authenticated()
 				.requestMatchers("/proxyBuyProducts", "/proxyBuyMain", "/proxyBuyBest", "/shareServiceBoard",
 						"/shareService","/chkFeed","/noneFeed")
