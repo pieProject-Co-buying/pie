6ba87b56348f0a2a3e5a159adc3c354c49b556f2
pieProjectMaster <pie030511@gmail.com>
2024-03-06 09:58:02 +0900

    [24.03.06]김혜정 sQL문 수정본 커밋

--- src/main/resources/static/SQL/Update/parti.sql
+++ src/main/resources/static/SQL/Update/parti.sql
@@ -1,8 +1,17 @@
 create table checkParticipate(
 boardNum number,
 tableName varchar2(20),
-writer varchar2(20),
-mem date,
+mem varchar2(20),
+partiDate date,
 status char(1),
 paid char(1)
+);
+
+select*from checkParticipate;
+
+DELETE FROM checkParticipate a
+WHERE ROWID > (
+    SELECT MIN(b.ROWID)
+    FROM checkParticipate b
+    WHERE a.boardNum = b.boardNum
 );
